197f95ef2c39aa73f11db2d9d1df98380bdfc7c3
jiho <jxxo.ih@gmail.com>
2023-10-20 10:16:46 +0900

    sav

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,60 @@
+import styles from "styles/Main.module.css";
+
+import mainImg from "assets/imgs/mainImg.jpg";
+import { useState } from "react";
+
 const Main = () => {
-    return ( 
+    return (
         <div>
-            main
+            <div className={styles.mainContent + " main"}>
+            </div>
+            <div className={styles.aboutWrap + " about"}>
+                <div className={styles.aboutTitle}>
+                    <p>
+                        About Me.
+                    </p>
+                </div>
+                <div className={styles.aboutContext}>
+                    <p className={styles.aboutContextTitle}>나는 강지호</p>
+                    <p className={styles.aboutContent}>개.발.자.다</p>
+                </div>
+            </div>
+            <div className={styles.skillWrap + " skills"}>
+                <div className={styles.skillTitle}>
+                    <p>
+                        Skills.
+                    </p>
+                </div>
+                <div className={styles.skillContent}>
+                    <div className={styles.skillList}>
+                        <div className={styles.skillBox}>
+                            <p className={styles.stackTitle}>Front.</p>
+                            <ul>
+                                <li className={styles.htmlIcon}>
+                                        <div className={styles.iconHover}>
+                                            html
+                                        </div>
+                                </li>
+                                <li className={styles.cssIcon}>CSS</li>
+                                <li className={styles.jsIcon}>JavaScript</li>
+                                <li className={styles.reactIcon}>React</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles.skillList}>
+                        <div className={styles.skillBox}>
+                            b
+                        </div>
+                    </div>
+                    <div className={styles.skillList}>
+                        <div className={styles.skillBox}>
+                            g
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Main;
--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -3,7 +3,12 @@ import styles from "styles/Header.module.css";
 const Header = () => {
     return (
         <div className={styles.gnb}>
-            
+            <ul>
+                <li>About.</li>
+                <li>Skills.</li>
+                <li>Project.</li>
+                <li>Contact.</li>
+            </ul>
         </div>
     );
 }
