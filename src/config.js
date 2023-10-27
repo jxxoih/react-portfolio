b6ad451057bd39fae2d955eebefcdc1301eac067
jiho <jxxo.ih@gmail.com>
2023-10-28 08:55:32 +0900

    서버연결 준비

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
 export const IMG_PATH = process.env.PUBLIC_URL + "/img/";
 
+const USE_LOCATION = window.location.href;
+
+export const API_PATH = USE_LOCATION.indexOf("localhost") < 0 ? "https://port-0-node-learning-12fhqa2llo7zh36o.sel5.cloudtype.app" : "http://localhost:3333";
+
 export const MOBILE_PX = 768;
