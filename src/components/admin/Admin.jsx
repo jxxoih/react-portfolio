acd1002c24fe5ce4e0f1add4080094072ed811e8
jiho <jxxo.ih@gmail.com>
2023-11-22 10:27:02 +0900

    관리자 페이지 비밀번호 체크 로직 수정

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,47 +1,9 @@
 import EnterPassword from "components/admin/EnterPassword";
-import { useState } from "react";
-import { useEffect } from "react";
+
 import AdminMain from "components/admin/AdminMain";
-import * as config from "config";
 
 const Admin = (props) => {
-    const { isAdmin, isMobile, aboutData, setAdmin, setPage, setLoading } = props;
-    const [password, setPassword] = useState("");
-
-    useEffect(() => {
-        checkedPassword();
-    }, [password]);
-
-    const passwordKeyDown = () => {
-        window.addEventListener("keydown", (e) => {
-            const checkCode = e.keyCode;
-            // 숫자, 문자만 입력 가능
-            if (checkCode === 13 || (checkCode >= 48 && checkCode <= 57) || (checkCode >= 65 && checkCode <= 90) || (checkCode >= 96 && checkCode <= 107)) {
-                if (e.key === "Enter") {
-                    if (password === config.ADMIN_PASSWORD) {
-                        setAdmin(true);
-                        setPage(true);
-                        e.preventDefault();
-                    } else {
-                        setAdmin(false);
-                        setPage(false);
-                        e.preventDefault();
-                    }
-                } else {
-                    setPassword(password + e.key);
-                }
-            }
-        })
-    }
-
-    const checkedPassword = () => {
-        if (isAdmin) {
-            setAdmin(true);
-        } else {
-            passwordKeyDown();
-        }
-
-    }
+    const { isAdmin, isMobile, aboutData, setAdmin, setPage, setLoading, isLoading } = props;
 
     return (
         <>
@@ -53,10 +15,15 @@ const Admin = (props) => {
                         aboutTitle: aboutData?.p_about_title,
                         aboutContext: aboutData?.p_about_context
                     }}
+                    isLoading={isLoading}
                 />
             )}
             {!isAdmin && (
-                <EnterPassword password={password} />
+                <EnterPassword
+                    isAdmin={isAdmin}
+                    setAdmin={setAdmin}
+                    setPage={setPage}
+                />
             )}
         </>
     );
--- src/components/admin/EnterPassword.jsx
+++ src/components/admin/EnterPassword.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import * as config from "config";
+import { useRef } from "react";
+
 const EnterPassword = (props) => {
     const colon = " : ";
     const regex = /./gi;
-    const {password} = props;
+    const { setAdmin, setPage, isAdmin } = props;
+
+    const [password, setPassword] = useState("");
+    var pwd = "";
+    const enterPwd = useRef("");
+
+    const setEnteredPwd = (arg) => {
+        if (!!arg) {
+            enterPwd.current = arg.toString();
+        } else {
+            enterPwd.current = "";
+        }
+        pwd = enterPwd.current;
+        setPassword(pwd)
+    }
+
+    const passwordKeyDown = (e) => {
+        const checkCode = e.keyCode;
+        // 숫자, 문자만 입력 가능
+        if (checkCode === 13 || (checkCode >= 48 && checkCode <= 57) || (checkCode >= 65 && checkCode <= 90) || (checkCode >= 96 && checkCode <= 107)) {
+            if (e.key === "Enter") {
+                if (pwd === config.ADMIN_PASSWORD) {
+                    setAdmin(true);
+                    setPage(true);
+                    e.preventDefault();
+                } else {
+                    setAdmin(false);
+                    setPage(false);
+                    e.preventDefault();
+                }
+                setEnteredPwd("");
+            } else {
+                setEnteredPwd(pwd + e.key);
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (isAdmin) {
+            setAdmin(true);
+            setEnteredPwd("");
+            return;
+        }
+
+        window.addEventListener('keydown', passwordKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", passwordKeyDown)
+        }
+    }, []);
 
     return (
         <div className="enterPwd">
