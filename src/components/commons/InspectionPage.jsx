f44611805137fd6856e30e5f69d6dd1124827d47
jiho <jxxo.ih@gmail.com>
2023-11-27 11:34:48 +0900

    점검중 페이지

--- src/components/commons/InspectionPage.jsx
+++ src/components/commons/InspectionPage.jsx
@@ -1,11 +1,22 @@
 import styles from "styles/modules/Inspection.module.css";
+import { IMG_PATH } from "config";
 
-const InspectionPage = () => {
-    return ( 
-        <div>
-            점검
+const warningImg = IMG_PATH + "warning.png";
+
+const InspectionPage = (props) => {
+    const { title, context } = props;
+
+    return (
+        <div className={styles.InspectionFrame}>
+            <div className={styles.InspectionWrap}>
+                <img src={warningImg} alt="warningImg" className={styles.warningImg} />
+                <p className={styles.InspectionTitle}>{title}</p>
+                {!!context && (
+                    <span className={styles.InspectionContext}>{context}</span>
+                )}
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default InspectionPage;
