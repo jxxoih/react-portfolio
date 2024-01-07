6d43f2d722df06c2d4d29bcceaafbb88061c7872
jiho <jxxo.ih@gmail.com>
2024-01-07 13:12:13 +0900

    오타 수정

--- src/components/commons/InspectionPage.jsx
+++ src/components/commons/InspectionPage.jsx
@@ -11,7 +11,7 @@ const InspectionPage = (props) => {
             <div className={styles.InspectionWrap}>
                 <img src={warningImg} alt="warningImg" className={styles.warningImg} />
                 <p className={styles.InspectionTitle}>
-                    {!!title ? title : "서비스 점겅중 입니다."}
+                    {!!title ? title : "서비스 점검중 입니다."}
                 </p>
                 {!!context && (
                     <span className={styles.InspectionContext}>{context}</span>
